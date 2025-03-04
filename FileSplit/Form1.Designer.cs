d9eab9ee2e223e95b289fa6c942f8460ae039056
jianghao1 <jianghao1@N-JIANGHAO1>
2025-03-05 07:33:52 +0800

    添加项目文件。

---
+++ FileSplit/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace FileSplit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button_select = new System.Windows.Forms.Button();
+            this.button_split = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(457, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button_select
+            // 
+            this.button_select.Location = new System.Drawing.Point(489, 12);
+            this.button_select.Name = "button_select";
+            this.button_select.Size = new System.Drawing.Size(75, 23);
+            this.button_select.TabIndex = 1;
+            this.button_select.Text = "选择";
+            this.button_select.UseVisualStyleBackColor = true;
+            this.button_select.Click += new System.EventHandler(this.button_select_Click);
+            // 
+            // button_split
+            // 
+            this.button_split.Location = new System.Drawing.Point(13, 61);
+            this.button_split.Name = "button_split";
+            this.button_split.Size = new System.Drawing.Size(75, 23);
+            this.button_split.TabIndex = 2;
+            this.button_split.Text = "分割";
+            this.button_split.UseVisualStyleBackColor = true;
+            this.button_split.Click += new System.EventHandler(this.button_split_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(407, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 110);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_split);
+            this.Controls.Add(this.button_select);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button_select;
+        private System.Windows.Forms.Button button_split;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FileSplit/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileSplit
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string FileName = "";
+
+        private void button_select_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                FileName = dialog.FileName;
+                textBox1.Text = FileName;
+            }
+        }
+
+        private async void button_split_Click(object sender, EventArgs e)
+        {
+            byte[] buffer = new byte[1024*1024*100];
+            //File.GetAttributes(FileName).
+            try
+            {
+                using (FileStream fs = new FileStream(FileName, FileMode.Open, FileAccess.Read))
+                {
+                    int bytesRead;
+                    int index = 0;
+                    long hasRead = 0;
+                    while ((bytesRead = await fs.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                    {
+                        // 处理读取到的数据
+                        hasRead += bytesRead;
+                        index++;
+                        label1.Text = (100.0* hasRead / fs.Length).ToString();
+                        string text = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                        string dir = Path.GetDirectoryName(FileName);
+                        File.WriteAllText(dir+"/split_" + index + ".log", text);
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("文件未找到。");
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show($"读取文件时发生错误: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"发生未知错误: {ex.Message}");
+            }
+
+        }
+    }
+}
